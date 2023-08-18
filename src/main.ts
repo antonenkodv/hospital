9ad4e8b28c8ea31924665b1f63a301b910f8c051
antonenkodv <morethanhyper@gmail.com>
2023-08-18 11:18:14 +0300

    -get port env variable for launch server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const configService = app.get(ConfigService);
 
     /** Setup Global Pipes */
     app.useGlobalPipes(
@@ -12,7 +15,8 @@ async function bootstrap() {
         }),
     );
 
-    const PORT = 3000;
+    const PORT = configService.get<number>('PORT');
+
     app.listen(PORT).then(() => console.log(`Server listening on port ${PORT}`));
 }
 bootstrap();
